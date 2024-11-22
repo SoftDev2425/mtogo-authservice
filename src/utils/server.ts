56dab3dc8ec562dda3b8b80a882f4d7cfdbe4aed
Andreas Fritzbøger <andreasfritzboger@live.dk>
2024-11-22 13:24:12 +0100

    Trying to make redis container work in pr.yml

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import routes from '../routes';
-// import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { logRequestDetails } from '../middlewares/loggerMiddleware';
 
@@ -8,12 +7,7 @@ function createServer() {
   const app = express();
 
   app.use(express.json());
-  // app.use(
-  //   cors({
-  //     credentials: true,
-  //     origin: ['http://localhost:3000'],
-  //   }),
-  // );
+
   app.use(cookieParser());
 
   app.use(logRequestDetails);
