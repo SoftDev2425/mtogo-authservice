15277da8b7a44948362ef9e306f20317c935ba9e
Owais Dashti <owaisdashti@outlook.com>
2024-11-27 15:33:27 +0100

    fix: cleanup

--- src/routes/restaurants.routes.ts
+++ src/routes/restaurants.routes.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import restaurantController from '../controllers/restaurant.controller';
 
-const  router = express.Router();
+const router = express.Router();
 
 router.get('/:restaurantId', restaurantController.handleGetRestaurantData);
 
